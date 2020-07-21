687f805cd6827882d78d04d160fcdf4834ea98e1
nikita <nilitagram@gmail.com>
2020-07-21 13:31:29 +0300

    fix https x3

--- src/utils/objectsHelper.js
+++ src/utils/objectsHelper.js
@@ -1,24 +1,22 @@
 import { API } from "../API/api";
 
 export const getDataFromArray = async (arr, name) => {
-    let newUrls = arr.map(url => {
-        return url.replace(/http/gi, 'https')
-     })
-    if(newUrls.length > 0) {
-        return await Promise.all(newUrls.map(async url =>  API.getInfo(url))).then(data => {
-            return  data.map(item => item.data[name])
+
+    if (arr.length > 0) {
+        return await Promise.all(arr.map(async url => API.getInfo(url))).then(data => {
+            return data.map(item => item.data[name])
         })
     }
-    
+
     return 'Unknown'
-}   
+}
 
 
-export const debounce = ( callback, data, delay ) => {
+export const debounce = (callback, data, delay) => {
     debugger
     let timeout;
     return () => {
-        clearTimeout( timeout );
-        timeout = setTimeout( callback(data), delay );
+        clearTimeout(timeout);
+        timeout = setTimeout(callback(data), delay);
     }
 }
