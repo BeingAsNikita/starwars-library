51cca90dc60263306ce7c795cdc271555ab74738
nikita <nilitagram@gmail.com>
2020-07-21 12:27:21 +0300

    Updates

---
+++ precache-manifest.33f5165fafe492b8a898bcd13228f3a8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d2d8c3ab4ce8baf378489407f40be9",
+    "url": "/starwars-library/index.html"
+  },
+  {
+    "revision": "b81c614484f5ffe98130",
+    "url": "/starwars-library/static/css/main.9ad37f33.chunk.css"
+  },
+  {
+    "revision": "c4fc4f1a4db2eaebefc0",
+    "url": "/starwars-library/static/js/2.f463eb6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starwars-library/static/js/2.f463eb6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81c614484f5ffe98130",
+    "url": "/starwars-library/static/js/main.33ff2b69.chunk.js"
+  },
+  {
+    "revision": "4d2af89bb59085afda8c",
+    "url": "/starwars-library/static/js/runtime-main.27562d3c.js"
+  },
+  {
+    "revision": "63f0c90107bb5080754d518568e618d4",
+    "url": "/starwars-library/static/media/birth_year.63f0c901.svg"
+  },
+  {
+    "revision": "33cdf1afcefc7255db4b4fce45b1eb75",
+    "url": "/starwars-library/static/media/films.33cdf1af.svg"
+  },
+  {
+    "revision": "65d3aac907a3a5e8b1cab3ae116a84c8",
+    "url": "/starwars-library/static/media/gender.65d3aac9.svg"
+  },
+  {
+    "revision": "52c98f0a88abf6082a6875324c036311",
+    "url": "/starwars-library/static/media/header-bg.52c98f0a.png"
+  },
+  {
+    "revision": "5102a4cb519074cc9ff08e6d5f989586",
+    "url": "/starwars-library/static/media/homeword.5102a4cb.svg"
+  },
+  {
+    "revision": "46b9ea745d7555abbbef7b22f8c0fc46",
+    "url": "/starwars-library/static/media/logo.46b9ea74.png"
+  },
+  {
+    "revision": "32560bd5be816d9c5f2c93cc6349f0a5",
+    "url": "/starwars-library/static/media/preloader.32560bd5.svg"
+  },
+  {
+    "revision": "772f0da57e57610f5ecf02b4d1395542",
+    "url": "/starwars-library/static/media/species.772f0da5.svg"
+  }
+]);
